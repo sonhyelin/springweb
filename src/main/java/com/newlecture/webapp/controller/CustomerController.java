b35d488f7c852ecbe38c038cad833faa346a1c83
user87 <user87@user87>
2017-10-20 09:02:15 +0900

    1019

--- src/main/java/com/newlecture/webapp/controller/CustomerController.java
+++ src/main/java/com/newlecture/webapp/controller/CustomerController.java
@@ -1,9 +1,6 @@
 package com.newlecture.webapp.controller;
-
 import java.util.List;
-
 import javax.sql.DataSource;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.google.gson.Gson;
 import com.newlecture.webapp.dao.NoticeDao;
 import com.newlecture.webapp.entity.NoticeView;
-
 /*@Controller
 @RequestMapping(value="/customer/*" , produces="text/html;charset=UTF-8")*/
 @Controller
@@ -25,7 +20,6 @@ public class CustomerController {
    
    @Autowired
    private NoticeDao noticeDao;
-
    @RequestMapping("notice")
    public String notice(@RequestParam(value="p", defaultValue="1") Integer page, 
          @RequestParam(value="f", defaultValue="title") String field,
@@ -67,7 +61,7 @@ public class CustomerController {
 	  
 	 /* StringBuilder builder = new StringBuilder();
 	  builder.append("[");
-	  builder.append("{}");
+	  builder.append("{}");x
 	  builder.append("{}");
 	  builder.append("]");
 	  
@@ -91,8 +85,8 @@ public class CustomerController {
       //return aaaid+"번째 공지사항 : "+noticeView.getTitle();
       
       model.addAttribute("n", noticeDao.get(id));
-      model.addAttribute("prev", noticeDao.getPrev(id));
-      model.addAttribute("next", noticeDao.getNext(id));
+      //model.addAttribute("prev", noticeDao.getPrev(id));
+      //model.addAttribute("next", noticeDao.getNext(id));
       
       
       return "customer.notice.detail";
--- src/main/java/com/newlecture/webapp/dao/spring/SpringNoticeDao.java
+++ src/main/java/com/newlecture/webapp/dao/spring/SpringNoticeDao.java
@@ -1,42 +1,120 @@
-/*package com.newlecture.webapp.dao.spring;
+package com.newlecture.webapp.dao.spring;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.newlecture.webapp.dao.NoticeDao;
+import com.newlecture.webapp.entity.Notice;
 import com.newlecture.webapp.entity.NoticeView;
 
 public class SpringNoticeDao implements NoticeDao {
-	
-	@Autowired
-	private DataSource dataSource;
+   
+   private JdbcTemplate template;
+   
+   public void setTemplate(JdbcTemplate template) {
+      this.template = template;
+   }
 
-	public List<NoticeView> getList(int page, String query) {
-		
-		return null;
-	}
+@Override
+   public List<NoticeView> getList(int page, String field, String query) {
+      
+      String sql = "select * from NoticeView" + 
+            "      where ? like ?" + 
+            "      order by regDate desc" + 
+            "      limit ?, 10";
+      
+      List<NoticeView> list = template.query(sql, new Object[] {"'%"+field+"%'","'%"+query+"%'", (page-1)*10}, BeanPropertyRowMapper.newInstance(NoticeView.class));
+      
+      return list;
+   }
 
-	public int getCount() {
-		return 0;
-	}
+   @Override
+   public int getCount() {
+      String sql = "select count(id) `count` from Notice";
+      
+      int count = template.queryForObject(sql, Integer.class);
+	   
+      return count;
+   }
 
-	public NoticeView get(String id) {
-		
-		String sql = "SELECT *FROM NoticeView WHERE id = ?";
-		
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		jdbcTemplate.queryForObject(sql, requiredType); //반환값이 리스트일경우 queryForObject이 아닌 query로 작성.
-		
-		return null;
-	}
+   @Override
+   public NoticeView get(String id) {
+      String sql = "select memberId id, pwd password from Notice where id=?";
+     
+      /*NoticeView notice = template.queryForObject(sql, new Object[] {id},
+            BeanPropertyRowMapper.newInstance(NoticeView.class));*/
+      
+      NoticeView notice = template.queryForObject(sql, new Object[]{id}, new RowMapper<NoticeView>(){
 
-	public int update(String id, String title, String content) {
-		return 0;
-	}
+      @Override
+      public NoticeView mapRow(ResultSet rs, int rowNum) throws SQLException {
+         
+         NoticeView notice = new NoticeView();
+         notice.setId(rs.getString("id"));
+         notice.setTitle(rs.getString("title")+"네오");
+         notice.setWriterId(rs.getString("writerId"));
+         notice.setContent(rs.getString("content"));
+         notice.setHit(0);
+         
+         return null;
+      }
+         
+         
+      });
+      
+      /*DriverManagerDataSource dataSource = new DriverManagerDataSource();
+      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+      dataSource.setUrl("jdbc:mysql://211.238.142.247/newlecture?autoReconnect=true&useSSL=false&useUnicode=true&=characterEncoding=utf8");
+      dataSource.setUsername("sist");
+      dataSource.setPassword("cclass");*/
+      
+     /* JdbcTemplate template = new JdbcTemplate();
+      template.setDataSource(dataSource);*/
+      
+   
+      return notice;
+   }
+
+   @Override
+   public int update(String id, String title, String content) {
+      // TODO Auto-generated method stub
+      return 0;
+   }
+
+   @Override
+   public NoticeView getPrev(String id) {      
+      
+      return null;
+   }
+
+   @Override
+   public NoticeView getNext(String id) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public int insert(String title, String content, String writerId) {
+      // TODO Auto-generated method stub
+      return 0;
+   }
+
+   @Override
+   public int insert(Notice notice) {
+      // TODO Auto-generated method stub
+      return 0;
+   }
+
+   @Override
+   public String getNextId() {
+      // TODO Auto-generated method stub
+      return null;
+   }
 
 }
-*/
